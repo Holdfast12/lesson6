04e5a2794131e74f50df92fd937ba056fbeabb61
Михаил <mih.kalyujniy@yandex.ru>
2022-12-18 21:07:23 +0300

    123

--- zadanie3.py
+++ zadanie3.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
 #XOR шифрование/расшифрование. На входе файл с текстом и ключ шифрования (строка), на выходе преобразованное (зашифрованное/расшифрованное) сообщение в файле.
 
+key = 67
+
+def inxor(text, t_key):
+    t_text = ''
+    for i in text:
+        t_text += chr(ord(i) ^ t_key)
+    return t_text
+
+with open('xor_original.txt', 'r') as file:
+    with open('xor_encrypted.txt', 'w') as file2:
+        file2.write(inxor(file.read(), key))
+
+with open('xor_encrypted.txt', 'r') as file:
+    print(inxor(file.read(), key))
