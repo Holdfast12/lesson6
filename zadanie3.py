96306dbf4a315d104f014a09e6cef2dd1f43fd41
Mikhail Kalyujniy <mih.kalyujniy@yandex.ru>
2022-12-19 11:25:05 +0300

    123

--- zadanie3.py
+++ zadanie3.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
 #XOR шифрование/расшифрование. На входе файл с текстом и ключ шифрования (строка), на выходе преобразованное (зашифрованное/расшифрованное) сообщение в файле.
 
-key = 67
+key = 'weqjfgjtyjtr'
 
-def inxor(text, t_key):
-    t_text = ''
-    for i in text:
-        t_text += chr(ord(i) ^ t_key)
-    return t_text
 
+def xorfunc(input_text, t_key):
+    """Функция, шифрующая/расшифрующая текст по алгоритму XOR. Принимает в качестве первого аргумента текст для шифрования/расшифрования, в качестве второго аргумента принимает ключ шифрования (строка).
+    """
+    t_text = list()
+    i = 0
+    for j in (ord(k) for k in input_text):
+        j ^= ord(t_key[i])
+        t_text.append(j)
+        i += 1
+        if i >= len(t_key):
+            i = 0
+    return ''.join(chr(k) for k in t_text)
+
+#чтение файла с исходным текстом xor_original.txt и запись файла с зашифрованным текстом 'xor_encrypted.txt'
 with open('xor_original.txt', 'r') as file:
     with open('xor_encrypted.txt', 'w') as file2:
-        file2.write(inxor(file.read(), key))
+        file2.write(xorfunc(file.read(), key))
 
+#чтение файла с зашифрованным текстом 'xor_encrypted.txt' и запись файла с расшифрованным текстом 'xor_transcribed.txt'
 with open('xor_encrypted.txt', 'r') as file:
-    print(inxor(file.read(), key))
+    with open('xor_transcribed.txt', 'w') as file2:
+        file2.write(xorfunc(file.read(), key))
